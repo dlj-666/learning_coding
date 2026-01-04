d6dc0835f67a6534ae41f5b9e266025f2546eedd
donrianjous <donrianjous@gmail.com>
2026-01-04 16:49:55 +0800

    平板和手机都可以了,另外，酣畅淋漓的战斗要来了

--- c/passage12/reverse.c
+++ c/passage12/reverse.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
-int main(void)
+#include <stdlib.h>
+int main(int argc, char *argv[])
 {
-    o
+    FILE *fp;
+    char ch;
+    long count, num;
+    if (argc < 2)
+    {
+        printf("Please input 2 arguments\n");
+        exit(1);
+    }
+    if ((fp = fopen(argv[1], "r+")) == NULL)
+    {
+        printf("no such file\n");
+        exit(1);
+    } 
+    fseek(fp, 0L, SEEK_END);
+    num = ftell(fp);
+    for (count = 2; count <= num;count++)
+    {
+        fseek(fp, -count, SEEK_END);
+        ch = getc(fp);
+        putc(ch, stdout);
+    }
+    printf("\n");
+    fclose(fp);
     return 0;
     }
---
+++ c/passage14/book.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define MAXTITL 41
+#define MAXAUTL 31
+int main(void)
+{
+    struct book {
+        char title[MAXTITL];
+        char author[MAXAUTL];
+        float price;
+    }
+    o
+    i
+    return 0;
+}
